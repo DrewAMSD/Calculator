207a6a1aba00fdc3b0109fbf5d8e21ac9b2d8cd0
DrewMiller2024 <drew.miller@mrjaffesclass.com>
2023-12-02 22:25:32 -0800

    changed where Number() is called

--- home.js
+++ home.js
@@ -1,14 +1,15 @@
 const addButton = document.getElementById("add1");
 addButton.addEventListener("click", add1);
+
 const subtractButton = document.getElementById("subtract1");
 subtractButton.addEventListener("click", subtract1);
 
 function add1() {
-  let divNum = document.getElementById("number").innerHTML;
-  document.getElementById("number").innerHTML = Number(divNum) + 1;
+  let divNum = Number(document.getElementById("number").innerHTML);
+  document.getElementById("number").innerHTML = divNum + 1;
 }
 
 function subtract1() {
-  let divNum = document.getElementById("number").innerHTML;
-  document.getElementById("number").innerHTML = Number(divNum) - 1;
+  let divNum = Number(document.getElementById("number").innerHTML);
+  document.getElementById("number").innerHTML = divNum - 1;
 }
