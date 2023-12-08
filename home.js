783bdabad78827ace935e2625903618eeb8a9341
DrewMiller2024 <drew.miller@mrjaffesclass.com>
2023-12-08 13:49:13 -0800

    add, subtract, multiply, and divide with 2 numbers, keeps track of ans

--- home.js
+++ home.js
@@ -1,20 +1,106 @@
 let main_number = document.getElementById("main_number");
 let operator = document.getElementById("operator");
-let operator_number = document.getElementById("operater_number");
-const plus_button = document.getElementById("plus_button");
+let second_number = document.getElementById("second_number");
+let answer = document.getElementById("answer");
+const buttons = document.querySelectorAll(".buttons");
+const operator_buttons = document.querySelectorAll(".operator_buttons");
+const equal_button = document.getElementById("equal_button");
 
-let num = 0;
-let num2 = 0;
-let operator_string = "";
+let num1 = "";
+let num2 = "";
+let ans = "";
+let operator_var = "";
 let second_operation = false;
+let reset_num1 = false;
 
-main_number.innerHTML = num;
-plus_button.addEventListener("click", change_operator_plus);
-add1Button.addEventListener("click", add1);
-subtract1Button.addEventListener("click", subtract1);
+equal_button.addEventListener("click", function (event) {
+  let num1_parse = Number(num1);
+  let num2_parse = Number(num2);
+  let doOperation = {
+    "+": function (x, y) {
+      return x + y;
+    },
+    "-": function (x, y) {
+      return x - y;
+    },
+    "*": function (x, y) {
+      return x * y;
+    },
+    "/": function (x, y) {
+      return x / y;
+    },
+  };
+  answer.innerHTML = num1 + " " + operator_var + " " + num2 + " =";
+  num1 = doOperation[operator_var](num1_parse, num2_parse);
+  num1 = num1.toString();
+  ans = num1;
+  operator_var = "";
+  num2 = "";
+  main_number.innerHTML = num1;
+  operator.innerHTML = "";
+  second_number.innerHTML = "";
+  second_operation = false;
+  reset_num1 = true;
+});
 
-function change_operator_plus() {
-  second_operation = true;
-  operator_string = "+";
-  operator.innerHTML = operator_string;
+buttons.forEach((button) => {
+  button.addEventListener("click", function (event) {
+    const buttonText = event.target.textContent;
+    if (!second_operation) {
+      if (reset_num1) {
+        num1 = "";
+        reset_num1 = false;
+      }
+      setNumDiv(1, buttonText);
+    } else {
+      setNumDiv(2, buttonText);
+    }
+  });
+});
+
+function setNumDiv(x, buttonText) {
+  answer.innerHTML = "ans = " + ans + " ";
+  let temp_num = x == 1 ? num1 : num2;
+  switch (buttonText) {
+    case ".":
+      if (!temp_num.includes(".")) {
+        temp_num += ".";
+      }
+      break;
+    case "(-)":
+      if (!temp_num.includes("-")) {
+        if (temp_num == "") {
+          temp_num = "-";
+        } else {
+          temp_num = "-" + temp_num;
+        }
+      } else {
+        temp_num = temp_num.substring(1);
+      }
+      break;
+    default:
+      if (temp_num == "") {
+        temp_num = buttonText;
+      } else {
+        temp_num += buttonText;
+      }
+      break;
+  }
+  if (x == 1) {
+    num1 = temp_num;
+    main_number.innerHTML = num1;
+  } else {
+    num2 = temp_num;
+    second_number.innerHTML = num2;
+  }
 }
+
+operator_buttons.forEach((operator_button) => {
+  operator_button.addEventListener("click", function (event) {
+    answer.innerHTML = "ans = " + ans + " ";
+    const operatorText = event.target.textContent;
+    second_operation = true;
+    operator_var = operatorText;
+    operator.innerHTML = operator_var;
+  });
+});
