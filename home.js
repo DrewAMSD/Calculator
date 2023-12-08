f482520a666ba489e93160a610e53ae614099804
DrewMiller2024 <drew.miller@mrjaffesclass.com>
2023-12-07 23:15:28 -0800

    number buttons, need to add event listeners

--- home.js
+++ home.js
@@ -1,18 +1,20 @@
+let main_number = document.getElementById("main_number");
+let operator = document.getElementById("operator");
+let operator_number = document.getElementById("operater_number");
+const plus_button = document.getElementById("plus_button");
+
 let num = 0;
-let main_number = (document.getElementById("main_number").innerHTML = num);
+let num2 = 0;
+let operator_string = "";
+let second_operation = false;
 
-const addButton = document.getElementById("add1");
-addButton.addEventListener("click", add1);
+main_number.innerHTML = num;
+plus_button.addEventListener("click", change_operator_plus);
+add1Button.addEventListener("click", add1);
+subtract1Button.addEventListener("click", subtract1);
 
-const subtractButton = document.getElementById("subtract1");
-subtractButton.addEventListener("click", subtract1);
-
-function add1() {
-  num = num + 1;
-  document.getElementById("main_number").innerHTML = num;
-}
-
-function subtract1() {
-  num = num - 1;
-  document.getElementById("main_number").innerHTML = num;
+function change_operator_plus() {
+  second_operation = true;
+  operator_string = "+";
+  operator.innerHTML = operator_string;
 }
